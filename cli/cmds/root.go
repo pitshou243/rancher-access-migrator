325c08d1cbf0392d387d06a061f4c9f916622acf
pitshou243 <pitshou.asingalembi@suse.com>
2026-09-25 09:01:47 -0500

    Add files via upload

---
+++ cli/cmds/root.go
@@ -0,0 +1,36 @@
+package cmds
+
+import (
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	Kubeconfig          string
+	TargetRancherConfig string
+	CommonFlags         = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "kubeconfig",
+			EnvVars:     []string{"KUBECONFIG"},
+			Usage:       "Kubeconfig path",
+			Destination: &Kubeconfig,
+		},
+		&cli.StringFlag{
+			Name:        "target-rancher-config",
+			EnvVars:     []string{"TARGET_RANCHER"},
+			Usage:       "(experimental) migrate cluster objects to another rancher deployment",
+			Destination: &TargetRancherConfig,
+		},
+	}
+	Spinner *spinner.Spinner
+)
+
+func NewApp() *cli.App {
+	app := cli.NewApp()
+	app.Name = "rancher-access-migrator"
+	app.Usage = "Tool for migrating rancher objects for RKE downstream clusters"
+	Spinner = spinner.New(spinner.CharSets[9], 100*time.Millisecond, spinner.WithColor("green"))
+	return app
+}
