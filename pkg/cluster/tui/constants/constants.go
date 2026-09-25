cb9cde80ef4e8fb1b0ecd7fcecfc8d56a02a3529
pitshou243 <pitshou.asingalembi@suse.com>
2026-09-25 09:18:41 -0500

    Add files via upload

---
+++ pkg/cluster/tui/constants/constants.go
@@ -0,0 +1,71 @@
+package constants
+
+import (
+	"github.com/pitshou243/rancher-access-migrator/pkg/client"
+	"github.com/pitshou243/rancher-access-migrator/pkg/cluster"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+/* CONSTANTS */
+const (
+	/* Migration Types */
+	MigratedStatus MigrationStatus = iota
+	WrongSpecStatus
+	NotMigratedStatus
+	CheckMark = "\u2714"
+	WrongMark = "\u2718"
+	WrongSpec = "(Wrong fields)"
+
+	/* Object Types */
+	ProjectsType   = "projects"
+	CRTBsType      = "crtbs"
+	NamespacesType = "namespaces"
+	PRTBsType      = "prtbs"
+	ReposType      = "repos"
+	ProjectType    = "project"
+	CRTBType       = "crtb"
+	NamespaceType  = "namespace"
+	PRTBType       = "prtb"
+	RepoType       = "repo"
+	UsersType      = "users"
+	UserType       = "user"
+)
+
+type MigrationStatus int
+
+var (
+	// P the current tea program
+	P *tea.Program
+	// SC the source cluster
+	SC *cluster.Cluster
+	// TC the target cluster
+	TC *cluster.Cluster
+	// Local Rancher Client
+	Lclient *client.Clients
+	// Target Rancher Client
+	TClient *client.Clients
+	// WindowSize store the size of the terminal window
+	WindowSize tea.WindowSizeMsg
+	// Migratedch all object chan
+	Migratedch = make(chan bool)
+	// logFile
+	LogFile os.File
+)
+
+/* STYLING */
+var (
+	DocStyle = lipgloss.NewStyle().Margin(0, 2).Foreground(lipgloss.Color("241"))
+
+	AppStyle = lipgloss.NewStyle().Padding(1, 2)
+
+	TitleStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Background(lipgloss.Color("#25A065")).
+			Padding(0, 1)
+	StatusMessageStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.AdaptiveColor{Light: "#04B575", Dark: "#04B575"}).
+				Render
+)
